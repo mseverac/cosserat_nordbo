fix pyel jacobian (pyel _control )

cosserat_nordbo/cosserat_rod_estimation/pyelastica_jacobian.py
@@ -6,7 +6,8 @@ from cosserat_nordbo.cosserat_rod_estimation.txt_reader import *
 from cosserat_nordbo.cosserat_rod_estimation.test_dx import compute_perturbed_inputs
 from copy import *
 
-
+def pp_list_to_s(pp_list):
+    return np.array(pp_list["position"][-1].transpose()).flatten()
 
 
 def compute_pyelastica_jacobian(start, end, R1, R2,
@@ -15,7 +16,12 @@ def compute_pyelastica_jacobian(start, end, R1, R2,
                                  final_time_init=0.04,
                                  final_time_ds=0.04,
                                  plot_cables=False,
-                                 points3d: np.ndarray = None):
+                                 plot_all=False,
+                                 points3d: np.ndarray = None,
+                                 last_jac = None,
+                                 dx = 0.01,
+                                 da = 0.1,
+                                 ):
 
     
     if points3d is None :
@@ -39,7 +45,11 @@ def compute_pyelastica_jacobian(start, end, R1, R2,
     else : 
         positions = points3d.transpose()
 
-    perturbed_pos = compute_perturbed_inputs(start,end,R1,R2,0.003,0.1)
+    perturbed_pos = compute_perturbed_inputs(start,end,R1,R2,dx,da)
+
+    drs = np.diag([dx,dx,dx,da,da,da,dx,dx,dx,da,da,da]) 
+    s0 = pp_list_to_s(pp_list)
+
 
     for i, (s, e, R1p, R2p) in enumerate(perturbed_pos):
 
@@ -49,18 +59,39 @@ def compute_pyelastica_jacobian(start, end, R1, R2,
         print(f"Start Rotation perturbed:\n{R1p}\n||| start : \n{R1}\n")
         print(f"End Rotation perturbed:\n{R2p}\n||| start : \n{R2}\n")
 """
-        pp_list1 = cosserat_get_cable_state(
-            s, e,
-            start_rotation=R1p, end_rotation=R2p,
-            final_time=final_time_ds,
-            damping_constant=1.0,
-            n_elem=n_elem,
 
-        )
+        if last_jac is not None :
+            print("computing initial position via last jacobian for cosserat get cable state")
+            dr = drs[i]
+            ds = last_jac @ dr.transpose()
+            s1 = s0 + ds
 
 
+            pp_list1 = cosserat_get_cable_state(
+                s, e,
+                start_rotation=R1p, end_rotation=R2p,
+                final_time=final_time_ds,
+                damping_constant=1.0,
+                n_elem=n_elem,
+                initial_position=s0.reshape(-1,3)
 
-        #plot_all_components(pp_list1, frames=frames)
+            )
+
+        else : 
+
+            pp_list1 = cosserat_get_cable_state(
+                s, e,
+                start_rotation=R1p, end_rotation=R2p,
+                final_time=final_time_ds,
+                damping_constant=1.0,
+                n_elem=n_elem,
+            )
+
+        s1 = pp_list_to_s(pp_list1)
+
+
+
+        if plot_all :plot_all_components(pp_list1)
 
         
 
@@ -73,7 +104,7 @@ def compute_pyelastica_jacobian(start, end, R1, R2,
 
             show_plot()
 
-        ds = (np.array(pp_list1["position"][-1]).flatten()-np.array(positions).flatten()).reshape(-1,1)
+        ds = ((s1-s0)/drs[i,i]).reshape(-1,1)
 
         if Jac is None :
             Jac = ds.copy()

cosserat_nordbo/pyel_control.py
@@ -34,6 +34,8 @@ class PyelControl(Node):
         super().__init__('pyel_control')
         self.A_pub = self.create_publisher(Float64MultiArray, '/jacobian', 10)
 
+        self.J = None
+
 
 
         self.create_subscription(
@@ -64,35 +66,50 @@ class PyelControl(Node):
     def publish_A(self):
         if hasattr(self, 'tcp_left') and hasattr(self, 'tcp_right'):
 
-            transform_right = self.tcp_right.transform
-            transform_left = self.tcp_left.transform
+            transform_right = self.tcp_right
+            transform_left = self.tcp_left
 
             before = self.get_clock().now().to_msg()
 
-            
-            Jac,curr_curve = compute_pyelastica_jacobian(
-            vector3_to_np(transform_right.transform.translation),
-            vector3_to_np(transform_left.transform.translation),
-            quaternion_to_rotmat(transform_right.transform.rotation),
-            quaternion_to_rotmat(transform_left.transform.rotation),
-            plot_cables=False,
-            n_elem = 50,
-            L=0.5,
-            )
+            if self.J is None :
+                Jac,curr_curve = compute_pyelastica_jacobian(
+                vector3_to_np(transform_right.transform.translation),
+                vector3_to_np(transform_left.transform.translation),
+                quaternion_to_rotmat(transform_right.transform.rotation),
+                quaternion_to_rotmat(transform_left.transform.rotation),
+                plot_cables=False,
+                n_elem = 50,
+                L=0.5,
+                )
+
+            else :
+                Jac,curr_curve = compute_pyelastica_jacobian(
+                vector3_to_np(transform_right.transform.translation),
+                vector3_to_np(transform_left.transform.translation),
+                quaternion_to_rotmat(transform_right.transform.rotation),
+                quaternion_to_rotmat(transform_left.transform.rotation),
+                plot_cables=False,
+                plot_all=True,
+                n_elem = 50,
+                L=0.5,
+                last_jac = self.J
+                ) 
+
 
             after = self.get_clock().now().to_msg()
             self.get_logger().info(f"Computation time of the jacobian: {time_msg_to_float(after) - time_msg_to_float(before):.4f}s")
 
 
-            Jp = np.linalg.pinv(Jac)
-            self.get_logger().info(f"shape of the jacobian: {Jp.shape}")
+            self.get_logger().info(f"shape of the jacobian: {Jac.shape}")
 
 
-            msg = Float32MultiArray()
+            msg = Float64MultiArray()
 
-            msg.data = Jp.flatten().astype(np.float32).tolist()
+            msg.data = Jac.flatten().astype(np.float32).tolist()
 
-            self.jac_pub.publish(msg)
+            self.A_pub.publish(msg)
+
+            self.J = Jac
 
 
         else: